f9a1047ee4ccd18b0e76dc7e1745279fa645069f
koyaaoyama <aoyama.ko0920@gmail.com>
2023-12-28 17:44:17 -0800

    Fix createDateTimeObject

--- src/features/api/event/services/index.ts
+++ src/features/api/event/services/index.ts
@@ -1,21 +1,37 @@
 import { AmPmString } from "@/types/event";
 
-/**
- * ex) new Date("2022-07-05"), 6, 5, "pm"
- * -> Date obj of 2022-07-05 18:05
- *
- * @param {Date} dateObject
- * @param {number} hour
- * @param {number} minutes
- * @param {AmPmString} ampm
- * @return {Date}
- */
-export const createDateTimeObject = (
-  dateObject: Date,
+type CreateDateTimeObjParams = {
+  year: number,
+  month: number,
+  day: number,
   hour: number,
   minutes: number,
   ampm: AmPmString
-): Date => {
+};
+
+/**
+ * ex) {2022, 7, 5, 6, 5, "pm}
+ * -> Date obj of 2022-07-05 18:05
+ *
+ *
+ * @param {CreateDateTimeObjParams} {
+ *   year,
+ *   month,
+ *   day,
+ *   hour,
+ *   minutes,
+ *   ampm,
+ * }
+ * @return {Date}
+ */
+export const createDateTimeObject = ({
+  year,
+  month,
+  day,
+  hour,
+  minutes,
+  ampm,
+}: CreateDateTimeObjParams): Date => {
   // 12 hours -> 24 hours
   let hour24 = hour;
   if (ampm === "pm" && hour < 12) {
@@ -24,8 +40,7 @@ export const createDateTimeObject = (
     hour24 = 0;
   }
 
-  const newDate = new Date(dateObject);
-  newDate.setHours(hour24, minutes, 0);
+  const newDate = new Date(year, month - 1, day, hour24, minutes, 0);
 
   return newDate;
 }
