3eb278dcbd95184dd7fd496e031729e6400680e5
koyaaoyama <aoyama.ko0920@gmail.com>
2024-01-13 23:30:53 -0800

    Apply devtools to scheduleStore

--- src/stores/scheduleStore.ts
+++ src/stores/scheduleStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 import { v4 as uuidv4 } from 'uuid';
 import { AmPmString } from '@/types/event';
 import { toYMDStr } from '@/utils';
@@ -16,7 +17,7 @@ export type TimeRange = {
 };
 
 type ScheduleDate = {
-  date: string; // ISO string
+  date: string; // yyyy-mm-dd string
   timeRanges: TimeRange[];
 };
 
@@ -29,39 +30,56 @@ interface ScheduleStore extends ScheduleState {
   removeDate: (dateToRemove: Date) => void;
   addTimeRangeToDate: (date: Date, newTimeRange: Omit<TimeRange, 'id'>) => void;
   removeTimeRangeFromDate: (date: Date, timeRangeToRemove: TimeRange) => void;
+  removeTimeRangeAndDateIfEmpty: (date: Date, timeRangeToRemove: TimeRange) => void;
 }
 
-export const useScheduleStore = create<ScheduleStore>(set => ({
-  scheduleDates: [],
+export const useScheduleStore = create<ScheduleStore>()(devtools(
+  set => ({
+    scheduleDates: [],
 
-  addDate: (newDate) =>
-    set((state) => {
-      const timeRanges: TimeRange[] = newDate.timeRanges.map(timeRange => ({ ...timeRange, id: uuidv4() }));
-      return {
-        scheduleDates: [...state.scheduleDates, { date: toYMDStr(newDate.date), timeRanges }]
-      };
-    }),
+    addDate: (newDate) =>
+      set((state) => {
+        const timeRanges: TimeRange[] = newDate.timeRanges.map(timeRange => ({ ...timeRange, id: uuidv4() }));
+        return {
+          scheduleDates: [...state.scheduleDates, { date: toYMDStr(newDate.date), timeRanges }]
+        };
+      }),
 
-  removeDate: (dateToRemove) =>
-    set((state) => ({
-      scheduleDates: state.scheduleDates.filter(date => date.date !== toYMDStr(dateToRemove))
-    })),
+    removeDate: (dateToRemove) =>
+      set((state) => ({
+        scheduleDates: state.scheduleDates.filter(date => date.date !== toYMDStr(dateToRemove))
+      })),
 
-  addTimeRangeToDate: (date, newTimeRange) =>
-    set((state) => ({
-      scheduleDates: state.scheduleDates.map(sd =>
-        sd.date === toYMDStr(date)
-          ? { ...sd, timeRanges: [...sd.timeRanges, { ...newTimeRange, id: uuidv4() }] }
-          : sd
-      )
-    })),
+    addTimeRangeToDate: (date, newTimeRange) =>
+      set((state) => ({
+        scheduleDates: state.scheduleDates.map(sd =>
+          sd.date === toYMDStr(date)
+            ? { ...sd, timeRanges: [...sd.timeRanges, { ...newTimeRange, id: uuidv4() }] }
+            : sd
+        )
+      })),
 
-  removeTimeRangeFromDate: (date, timeRangeToRemove) =>
-    set((state) => ({
-      scheduleDates: state.scheduleDates.map(sd =>
-        sd.date === toYMDStr(date)
-          ? { ...sd, timeRanges: sd.timeRanges.filter(tr => tr !== timeRangeToRemove) }
-          : sd
-      )
-    })),
-}));
+    removeTimeRangeFromDate: (date, timeRangeToRemove) =>
+      set((state) => ({
+        scheduleDates: state.scheduleDates.map(sd =>
+          sd.date === toYMDStr(date)
+            ? { ...sd, timeRanges: sd.timeRanges.filter(tr => tr !== timeRangeToRemove) }
+            : sd
+        )
+      })),
+
+    removeTimeRangeAndDateIfEmpty: (date: Date, timeRangeToRemove: TimeRange) =>
+      set((state) => {
+        // remove timeRange
+        const updatedScheduleDates = state.scheduleDates.map(sd =>
+          sd.date === toYMDStr(date)
+            ? { ...sd, timeRanges: sd.timeRanges.filter(tr => tr.id !== timeRangeToRemove.id) }
+            : sd
+        );
+
+        // remove date that has empty timeRanges
+        const finalScheduleDates = updatedScheduleDates.filter(sd => !(sd.date === toYMDStr(date) && sd.timeRanges.length === 0));
+
+        return { scheduleDates: finalScheduleDates };
+      }),
+}), { name: 'scheduleStore' }));
