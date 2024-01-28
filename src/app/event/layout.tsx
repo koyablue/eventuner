2ed9bf7d6bd8cef3255d4201454011e291a2b949
koyaaoyama <aoyama.ko0920@gmail.com>
2024-01-27 19:23:03 -0800

    Move outer elements to layout component

--- src/app/event/layout.tsx
+++ src/app/event/layout.tsx
@@ -1,7 +1,9 @@
 export default function NewEventPageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <main className="flex justify-center min-h-screen bg-slate-100 md:p-24">
+      <div className="w-full min-w-[320px] max-w-5xl min-h-[600px] bg-white px-8 rounded-md shadow-md lg:px-4 lg:h-[calc(100vh-96px-96px)]">
       {children}
-    </>
+      </div>
+    </main>
   );
 }
--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -134,12 +134,8 @@ const NewEvent = () => {
     setCalendarSelectedDays(updatedSelectedDays);
   }, [eventDates]);
 
-  // TODO: Loading view
-  // TODO: Separate form into another component
-
   return (
-    <main className="flex justify-center min-h-screen bg-slate-100 md:p-24">
-      <div className="w-full min-w-[320px] max-w-5xl min-h-[600px] bg-white px-8 rounded-md shadow-md lg:px-4 lg:h-[calc(100vh-96px-96px)]">
+    <>
         {isSubmitted
           ? <EventCreated uuid={createdEventUuid} />
           : <EventForm
@@ -154,8 +150,7 @@ const NewEvent = () => {
               submitAction={callCreateEventAction}
             />
         }
-      </div>
-    </main>
+    </>
   )
 }
 
