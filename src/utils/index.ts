2433cb745c3cbc272e772b3959c68713966c8f0a
koyaaoyama <aoyama.ko0920@gmail.com>
2024-01-28 00:23:12 -0800

    Implement toOrdinal, getOrdinalSuffix util functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -23,3 +23,32 @@ export const extract12HourFormat = (date: Date): { hour: number, minutes: number
  * @return {string}
  */
 export const toYMDStr = (date: Date): string => format(date, 'y-MM-dd');
+
+/**
+ * Get ordinal suffix of num
+ * ex) num = 1 -> "st"
+ *
+ * @param {number} num
+ * @return {string}
+ */
+export const getOrdinalSuffix = (num: number): string => {
+  let suffix = "th";
+
+  if (num % 10 === 1 && num % 100 !== 11) {
+    suffix = "st";
+  } else if (num % 10 === 2 && num % 100 !== 12) {
+    suffix = "nd";
+  } else if (num % 10 === 3 && num % 100 !== 13) {
+    suffix = "rd";
+  }
+
+  return suffix;
+};
+
+/**
+ * Convert number to ordinal number
+ *
+ * @param {number} num
+ * @return {string} 1st, 2nd, 3rd etc.
+ */
+export const toOrdinal = (num: number): string => `${num}${getOrdinalSuffix(num)}`;
