4fea319535ba0cd05dc558ceddff2db4189caf8c
koyaaoyama <aoyama.ko0920@gmail.com>
2024-02-03 01:49:12 -0800

    Change env var name. NEXT_PUBLIC_API_BASE_URL -> API_BASE_URL

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,11 +4,11 @@
  * @return {string}
  */
 export const getApiBaseUrl = (): string => {
-  if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
-    throw new Error('NEXT_PUBLIC_API_BASE_URL is undefined');
+  if (!process.env.API_BASE_URL) {
+    throw new Error("API_BASE_URL is undefined");
   }
 
-  return process.env.NEXT_PUBLIC_API_BASE_URL;
+  return process.env.API_BASE_URL;
 };
 
 /**
